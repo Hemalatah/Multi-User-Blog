9ac42f3b758c6125ff1af029c2605a7a059486d4
Hemalatha Kottaisamy <khema2010@gmail.com>
2017-03-09 23:59:39 -0800

    fix: pep8 correction

--- models/comments.py
+++ models/comments.py
@@ -2,10 +2,11 @@ from users import *
 
 from google.appengine.ext import db
 
+
 class Comment(db.Model):
-    comment = db.StringProperty(required = True)
-    post = db.StringProperty(required = True)
-    blogger = db.StringProperty(required = True)
+    comment = db.StringProperty(required=True)
+    post = db.StringProperty(required=True)
+    blogger = db.StringProperty(required=True)
 
     @classmethod
     def render(self):
--- models/posts.py
+++ models/posts.py
@@ -9,29 +9,33 @@ from google.appengine.ext import db
 template_dir = os.path.join(os.getcwd(), 'templates/')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
+
+
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
-def blog_key(name = 'default'):
-	return db.Key.from_path('blogs', name)
+
+def blog_key(name='default'):
+    return db.Key.from_path('blogs', name)
+
 
 class Post(db.Model):
-	title = db.StringProperty(required = True)
-	story = db.TextProperty(required = True)
-	created = db.DateTimeProperty(auto_now_add = True)
-	last_modified = db.DateTimeProperty(auto_now = True)
-	blogger = db.StringProperty()
-	liked_by = db.ListProperty(str)
+    title = db.StringProperty(required=True)
+    story = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+    blogger = db.StringProperty()
+    liked_by = db.ListProperty(str)
 
-	def render(self):
-		self._render_text = self.story.replace('\n', '<br>')
-		return render_str('post.html', p = self)
+    def render(self):
+        self._render_text = self.story.replace('\n', '<br>')
+        return render_str('post.html', p=self)
 
-	@property
-	def likes(self):
-		return len(self.liked_by)
+    @property
+    def likes(self):
+        return len(self.liked_by)
 
-	@property
-	def comments(self):
-		return Comment.all().filter("post = ", str(self.key().id()))
+    @property
+    def comments(self):
+        return Comment.all().filter("post = ", str(self.key().id()))
